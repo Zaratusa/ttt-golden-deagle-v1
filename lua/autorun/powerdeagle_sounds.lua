cb519759e190eac7f14ffcca9d32e0a17cc78618
Zaratusa <zaratusa.codec@gmail.com>
2020-07-01 03:35:41 +0200

    Initial commit

---
+++ lua/autorun/powerdeagle_sounds.lua
@@ -0,0 +1,9 @@
+sound.Add({
+	name = "Golden_Deagle.Single",
+	channel = CHAN_WEAPON,
+	volume = 1.0,
+	pitch = {95, 105},
+	sound = "weapons/golden_deagle/deagle-1.wav"
+})
+
+game.AddParticles("particles/smoke_trail.pcf")
